2df62526ae202305e859d74a961580ff26a3c3b5
JenniferGlassmyer <jennifer.glassmyer@gmail.com>
2021-09-09 12:09:32 +0200

    added logic for sorting results by zip code

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -4,14 +4,15 @@ require 'open-uri'
 class MuseumsController < ApplicationController
   def index
     @museums = find_museums(52.494857, 13.437641)
+    @postal_codes = []
+    @museums["features"].each do |feature|
+      @postal_codes << feature['context'][0]['text']
+    end
   end
 
   def find_museums(latitude, longitude)
     url = "https://api.mapbox.com/geocoding/v5/mapbox.places/museums.json?lat=#{latitude}&lng=#{longitude}&access_token=pk.eyJ1IjoiamVubnlnbGFzc215ZXIiLCJhIjoiY2tycWNjaG95MXlrYTJvbDc2cHVtMXQ0MSJ9.DLY9_kAeCbnFyDmNdqvO-w"
     museums_serialized = URI.open(url).read
-    museums = JSON.parse(museums_serialized)
-    museums["features"].each do |feature|
-      
-    end
+    JSON.parse(museums_serialized)
   end
 end
